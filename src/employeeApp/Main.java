79ab14b2451f3162e98ced55f82890d229058529
Mehmet <mehmetsonmez216@gmail.com>
2023-11-20 03:06:52 +0300

    all project finished

--- src/employeeApp/Main.java
+++ src/employeeApp/Main.java
@@ -1,5 +1,12 @@
 package employeeApp;
 
+import employeeApp.enums.Plan;
+import employeeApp.model.Company;
+import employeeApp.model.Employee;
+import employeeApp.model.Healthplan;
+
+import java.util.Arrays;
+
 /**
  * Uygulamanın amacı Java'da nesne tabanlı programlamanın temellerinin oluşturulmasıdır.
  *
@@ -10,8 +17,32 @@ public class Main
     /**
      * Uygulamadaki tüm işlemler bu metotda yapılmalı
      */
+
     private static void workWithData ()
     {
+        Healthplan healthplan1=new Healthplan(1,"A Sigorta", Plan.ADVANCE);
+        Healthplan healthplan2=new Healthplan(2,"B Sigorta", Plan.MEDIUM);
+
+        String[] healtplans = new String[4];
+        healtplans[0] = healthplan1.getName();
+        healtplans[1] = healthplan2.getName();
+
+        Employee employee1 =new Employee(1,"mehmet","msmsm@mhm","423l534l5",healtplans);
+        employee1.addHealthplan(2,"C Sigorta");
+        employee1.addHealthplan(2,"C Sigorta");
+        System.out.println(Arrays.toString(employee1.getHealthPlans()));
+
+
+        Company company = new Company(1, "Workintech"
+                , new String[]{"Dogancan Kınık", "Omer Fatih Tanriverdi", null});
+
+        company.addEmployee(0, "Ali Veli");
+        company.addEmployee(2, "Mehmet Ali");
+        company.addEmployee(-1, "Mehmet Ali");
+        company.addEmployee(5, "Mehmet Ali");
+
+        System.out.println(Arrays.toString(company.getDeveloperNames()));
+        System.out.println(company);
 
     }
 
